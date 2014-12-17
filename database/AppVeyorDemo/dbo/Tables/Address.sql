26830ceab4b218762695e92c9b76d3ee24f2497e
Jonathan Hickford <jonathan.hickford@gmail.com>
2014-12-17 11:36:50 +0000

    added database project

---
+++ database/AppVeyorDemo/dbo/Tables/Address.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Address] (
+    [id]          INT           IDENTITY (1, 1) NOT NULL,
+    [line1]       NVARCHAR (50) NULL,
+    [line2]       NVARCHAR (50) NULL,
+    [postcode]    NVARCHAR (50) NULL,
+    [Customer_id] INT           NULL,
+    CONSTRAINT [PK_Address] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_Cusomer_Address] FOREIGN KEY ([Customer_id]) REFERENCES [dbo].[Customer] ([Id])
+);
+
---
+++ database/AppVeyorDemo/dbo/Tables/Customer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (50) NULL,
+    [Age]          INT           NULL,
+    [FirstName]    NVARCHAR (50) NULL,
+    [LastName]     NVARCHAR (50) NULL,
+    [phone number] NVARCHAR (20) NULL,
+    CONSTRAINT [PK__Customer__738F19E0] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
